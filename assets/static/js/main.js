bcffe1d17ea45185707c4484417e59e7505be82c
uqrock <yury.beloborodov@gmail.com>
2019-05-03 15:10:43 +0300

    add yandex maps + footer-popups

--- assets/static/js/main.js
+++ assets/static/js/main.js
@@ -110,6 +110,16 @@ $(document).ready(function () {
       });
    };
 
+   var contactsPopup = function () {
+      $(document).on('click', '.contacts-popup__toogle', function () {
+         console.log('sd');
+         $(this).parent().addClass('contacts-popup--active');
+      });
+      $(document).on('click', '.contacts-popup__close', function () {
+         $(this).closest('.contacts-popup').removeClass('contacts-popup--active');
+      });
+   }
+
    sandwich();
    popularCategoriesSlider();
    productPrevSlider();
@@ -117,6 +127,7 @@ $(document).ready(function () {
    popupLink();
    formValidation();
    reviewLine();
+   contactsPopup();
 });
 
 var popularCategoriesSlider = function () {
@@ -135,3 +146,47 @@ var popularCategoriesSlider = function () {
 $(window).on('resize', function () {
    popularCategoriesSlider();
 });
+
+if ($('div').is('.contacts-popup__map')) {
+   ymaps.ready(function () {
+      var myMapOffice = new ymaps.Map('popup-contacts-office', {
+              center: [55.754578, 37.694953],
+              zoom: 14
+          }, {
+              searchControlProvider: 'yandex#search'
+          }),
+          myPlacemark1 = new ymaps.Placemark(myMapOffice.getCenter(), {
+              hintContent: 'Собственный значок метки',
+              balloonContent: 'Это красивая метка'
+          }, {
+              iconLayout: 'default#image',
+              iconImageHref: 'images/general/map-pin.svg',
+              iconImageSize: [56, 69],
+              iconImageOffset: [-5, -38]
+          });
+
+      myMapOffice.geoObjects.add(myPlacemark1);
+      myMapOffice.behaviors.disable('scrollZoom');
+      myMapOffice.controls.remove('trafficControl').remove('searchControl').remove('typeSelector').remove('geolocationControl').remove('fullscreenControl').remove('rulerControl');
+
+      var myMapStock = new ymaps.Map('popup-contacts-stock', {
+               center: [55.566373, 37.55856],
+               zoom: 14
+            }, {
+               searchControlProvider: 'yandex#search'
+            }),
+            myPlacemark2 = new ymaps.Placemark(myMapStock.getCenter(), {
+               hintContent: 'Собственный значок метки',
+               balloonContent: 'Это красивая метка'
+            }, {
+               iconLayout: 'default#image',
+               iconImageHref: 'images/general/map-pin.svg',
+               iconImageSize: [56, 69],
+               iconImageOffset: [-5, -38]
+            });
+
+      myMapStock.geoObjects.add(myPlacemark2);
+      myMapStock.behaviors.disable('scrollZoom');
+      myMapStock.controls.remove('trafficControl').remove('searchControl').remove('typeSelector').remove('geolocationControl').remove('fullscreenControl').remove('rulerControl');
+  });
+}
